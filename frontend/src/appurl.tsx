5a95188eadcb5ed68bd451bfb43ef780ae4c699e
Bethelem Melese <bethelem.m@ambftradingplc.com>
2025-04-04 22:06:31 +0300

    Update the content

--- frontend/src/appurl.tsx
+++ frontend/src/appurl.tsx
@@ -1,4 +1,5 @@
-export const appUrl = "https://invest-with-brook-real-estate.vercel.app/api/";
-// export const appUrl = "http://localhost:5000/api/";
+// export const appUrl = "https://invest-with-brook-real-estate.vercel.app/api/";
+export const appUrl = "http://localhost:5000/api/";
 
 export const token = localStorage.getItem("token");
+// 
